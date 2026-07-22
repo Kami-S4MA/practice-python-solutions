52fef5d3fecbec8ce9994d04197f14318be04ec7
Kami-S4MA <shivanshushuklathe1@gmail.com>
2026-07-22 16:26:12 +0530

    Problem 21: Write To a File

---
+++ 21. Write To A File/solution.py
@@ -0,0 +1,28 @@
+import requests
+from bs4 import BeautifulSoup
+
+base_url = "http://www.vanityfair.com/society/2014/06/monica-lewinsky-humiliation-culture"
+
+headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"}
+
+file = open(input("Enter filename: ")+".txt", "a")
+
+for page_num in range(1, 5):
+  
+  if page_num == 1:
+    url = base_url
+  else:
+    url = base_url+"/"+str(page_num)
+  
+  r = requests.get(url, headers=headers)
+  
+  soup = BeautifulSoup(r.text, 'html.parser')
+  
+  file.write("\nPAGE"+str(page_num)+"\n")
+  
+  all_paragraphs = soup.find_all('p')
+  
+  for i in all_paragraphs:
+    text = i.text.strip()
+    if len(text) > 40:
+      file.write(text+"\n")
